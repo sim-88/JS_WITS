2235a24bf301d37bd684fa352a699c9cc64327ac
Simarpreet Kaur <simarpreet.kaur@marblex.ai>
2025-01-06 17:55:04 +0530

    variables, hoisting

---
+++ Day3/Operations.js
@@ -0,0 +1,48 @@
+let num=5;
+
+let result=num+"10";
+
+
+let r2=num+"apples";
+
+
+let r3="string"+undefined;
+
+
+let r4=10+Infinity;
+console.log(r4);
+
+let obj={name:"John",car:"BMW"};
+let str="User: ";
+console.log(obj+str);
+
+let str2="newstr";
+
+let r5=num-str2;
+console.log(r5);
+
+let r6=num*str2;
+console.log(r6);
+
+
+let r7=num/str2;
+console.log(r7);
+
+let r8="A"<"B";
+console.log(r8);
+
+console.log(5&1);
+console.log(5|1);
+console.log(~5);
+console.log(5^1);
+console.log(5<<1);
+
+console.log(5==5);
+console.log(5==="5");
+
+
+
+
+
+
+
---
+++ Day3/variables.js
@@ -0,0 +1,27 @@
+{
+    var x=2;
+}
+
+{
+    let y=2;
+}
+console.log(x);  //var has global scope
+//console.log(y);   //let has Block scope
+// ReferenceError
+
+var x=12;
+
+console.log(x);
+
+
+//Hoisting in var
+carName="Volvo";
+var carName;
+
+
+// letName="new";  //Reference Error TDZ
+// let letName;
+
+
+
+
